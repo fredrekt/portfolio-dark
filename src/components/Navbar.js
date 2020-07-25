d85f67280c5ea8a4a6b73cdef6eb666f3838b7be
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-07-25 12:04:28 +0800

    added styles to the links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,43 +23,56 @@ class Navbar extends React.Component {
     const theme = this.props.color 
     const color = theme === 'dark' ? "#fff" : "#000"
     const styleColor = {
-      color: color
+      color: color,
+      fontWeight: `700`,
+      fontSize: `1rem`,
+      textTransform: `uppercase`,
+      fontFamily:`'Lato', sans-serif!important`,
+      letterSpacing: `0.3em`
+    }
+
+    const logo = {
+      color: color,
+      fontWeight: `900`,
+      fontSize: `2rem`
     }
 
     return (
       <div>
         <header>
-              <MDBNavbar className="z-depth-0 p-5" scrolling color="light" light dark expand="md">
+              <MDBNavbar className="z-depth-0 py-5" scrolling color="light" light dark expand="md">
                 <MDBContainer fluid>
-                  <Link style={styleColor} to="/">
-                    <strong>Navbar</strong>
+                  <Link to="/">
+                    <strong style={logo} className="special-font">
+                      Spicy Developer
+                    </strong>
                   </Link>
                   <MDBNavbarToggler onClick={this.onClick} />
                   <MDBCollapse isOpen={this.state.collapse} navbar>
                     <MDBNavbarNav className="align-items-center" right>
                       <MDBNavItem>
-                        <Link style={styleColor} className="px-5" to="/about">
+                        <Link style={styleColor} className="px-4" to="/about">
                           About
                         </Link>
                       </MDBNavItem>
                       <MDBNavItem>
-                        <Link style={styleColor} className="px-5"to="/works">
+                        <Link style={styleColor} className="px-4"to="/works">
                           Work
                         </Link>
                       </MDBNavItem>
                       <MDBNavItem>
-                        <Link style={styleColor} className="px-5"to="/resume">
+                        <Link style={styleColor} className="px-4"to="/resume">
                           Resume
                         </Link>
                       </MDBNavItem>
                       <MDBNavItem>
-                        <Link style={styleColor} className="px-5"to="/contact">
+                        <Link style={styleColor} className="px-4"to="/contact">
                           Contact
                         </Link>
                       </MDBNavItem>
                       <MDBNavItem>
-                        <Button className="px-5" kind={KIND.minimal} onClick={this.props.onClick}>
-                          Dark or Light
+                        <Button style={styleColor} className="px-4" kind={KIND.minimal} onClick={this.props.onClick}>
+                          {theme === 'light' ? "Toggle Dark": "Toggle Light"}
                         </Button>
                       </MDBNavItem>
                     </MDBNavbarNav>
