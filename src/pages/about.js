c53edc807f75f505516da965eb1f8ce1a95367e2
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-08-03 09:10:17 +0800

    mobile ready

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import SEO from '../components/seo'
 import HeaderPage from '../components/HeaderPage';
 import { MDBContainer, MDBRow, MDBCol } from 'mdbreact'
 import { ThemeProvider, DarkTheme, LightTheme } from 'baseui';
 import Navbar from '../components/Navbar';
 import { Parallax } from 'react-parallax';
-import { StyledLink } from "baseui/link";
+import {styled} from 'baseui';
 
 const THEME = {
     light: 'light',
@@ -18,10 +18,47 @@ const AboutPage = () => {
     }
     const selectedTheme =  localStorage.getItem('themeColor');
     const [theme, setTheme] = useState(selectedTheme === THEME.light ? THEME.light : THEME.dark);
-    const content = {
+
+    const AboutHeader = styled('h1', {
+        fontSize: `3.25rem`,
+        lineHeight: `1.4`,
+        "@media screen and (max-width: 540px)": {
+            fontSize: `2.25rem`
+        }
+    })
+
+    const AboutContent = styled('p', {
         fontSize: `1.3rem`,
-        fontFamily: `'Biryani', sans-serif`
-    }
+        fontFamily: `'Biryani', sans-serif`,
+        "@media screen and (max-width: 540px)": {
+            fontSize: `1rem`
+        }
+    })
+
+    const AboutStyledLink = styled('a', {
+        fontSize: `1.3rem`,
+        fontFamily: `'Biryani', sans-serif`,
+        color: `#000`,
+        textDecoration: `underline`,
+        textUnderlinePosition: `under`,
+        ":hover":{
+            color: `#000`
+        },
+        "@media screen and (max-width: 540px)": {
+            fontSize: `1rem`
+        }
+    })
+
+    const LocationContainer = styled('div', {
+        width: `50%`,
+        marginLeft: `auto`,
+        marginRight: `auto`,
+        marginTop: `3rem`,
+        textAlign: `center`,
+        "@media screen and (max-width: 540px)": {
+            width: `100%`
+        }
+    })
 
     return (
         <ThemeProvider theme={theme === THEME.light ? LightTheme : DarkTheme}>
@@ -32,7 +69,7 @@ const AboutPage = () => {
           } color={theme}/>
             <MDBContainer fluid className="px-4 pb-5">
                 <HeaderPage text="Me, Myself & I"/>
-                <MDBRow>
+                <MDBRow className="pb-5 pb-lg-0 pb-md-0">
                     <MDBCol>
                         <Parallax
                         strength={800}
@@ -41,30 +78,30 @@ const AboutPage = () => {
                         </Parallax>
                     </MDBCol>
                 </MDBRow>
-                <MDBRow className="my-5 py-5">
+                <MDBRow className="my-lg-5 my-0 py-lg-5 py-0">
                     <MDBCol md="6">
-                        <h1 style={{ fontSize: `3.25rem`, lineHeight: `1.4` }}>
+                        <AboutHeader>
                             I am a Full Stack Developer, love JavaScript 
                             and also play first person shooter games.
-                        </h1>
+                        </AboutHeader>
                     </MDBCol>
                     <MDBCol md="6">
                         <div className="mt-2">
-                            <p style={content} className="lead">
+                            <AboutContent className="lead">
                             Mainly i love react. Would love to learn more about JavaScript. 
                             Absolute would love to discover new technologies and build things 
                             on my own. That's how i learn. Love to playing Apex Legends.
                             Listening to music while i work equals productivity for me. 
-                            </p>
-                            <p style={content} className="lead">
+                            </AboutContent>
+                            <AboutContent className="lead">
                             If you have a project or an idea in mind, don't be shy and send me  
-                            a <StyledLink style={content} href="/contact">message here</StyledLink>, from there let’s see where it takes us.
-                            </p>
+                            a <AboutStyledLink href="/contact">message here</AboutStyledLink>, from there let’s see where it takes us.
+                            </AboutContent>
                         </div>
                     </MDBCol>
                 </MDBRow>
-                <MDBRow className="my-5 pt-5">
-                    <MDBCol md="6" lg="6">
+                <MDBRow className="my-0 my-lg-5 my-md-5 pt-lg-5 pt-md-5 pt-0">
+                    <MDBCol className="my-4 my-lg-0 my-md-0" md="6" lg="6">
                         <img src={`https://images.unsplash.com/photo-1583492207605-884991f34b2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80`}
                         className="w-100"
                         alt=""/>
@@ -82,11 +119,11 @@ const AboutPage = () => {
                             title="developer's location" 
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15703.872331734146!2d123.81861022739237!3d10.264155282724873!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a99d08fc35c237%3A0x9841502ea3d82016!2sLawaan%20III%2C%20Talisay%2C%20Cebu!5e0!3m2!1sen!2sph!4v1595642283821!5m2!1sen!2sph" width="100%" height="400" frameborder="0" style={{"border":0}} allowfullscreen="" aria-hidden="false"></iframe>
                         </div>
-                        <div className="mt-5 mx-auto text-center w-50">
-                            <h1 style={{ fontSize: `3.25rem`, lineHeight: `1.4` }}>
+                        <LocationContainer>
+                            <AboutHeader>
                                 Lawaan III - Talisay City, Central Visayas, Philippines
-                            </h1>
-                        </div>
+                            </AboutHeader>
+                        </LocationContainer>
                     </MDBCol>
                 </MDBRow>
             </MDBContainer>
