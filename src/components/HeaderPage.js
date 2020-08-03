a7166612deea344a788bbc0607b5009224f61301
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-08-03 09:10:36 +0800

    added font responsive

--- src/components/HeaderPage.js
+++ src/components/HeaderPage.js
@@ -4,7 +4,10 @@ import {styled} from 'baseui';
 const Header = styled('h1', ({$theme})=>({
     fontSize: `5rem`,
     fontFamily: `'Lato', sans-serif`,
-    letterSpacing: `-.01em`
+    letterSpacing: `-.01em`,
+    "@media screen and (max-width: 540px)": {
+        fontSize: `3rem`
+    }
 })); 
 
 const HeaderPage = ({text}) => (<Header className="my-5">{text}</Header>)
