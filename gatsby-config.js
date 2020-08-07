c498c65bb9f322992311d776d56db3951f443b47
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-08-07 17:24:11 +0800

    GraphCMS source added

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,18 @@ module.exports = {
       },
     },
     {
+      resolve: "gatsby-source-graphql",
+          options: {
+          // The top level query type, can be anything you want!
+          typeName: "GCMS",
+          // The field you'll query against, can also be anything you want.
+          fieldName: "gcms",
+          // Your API endpoint, available from the dashboard and settings window.
+          // You can use this endpoint that features US mountains for now.
+          url: "https://api-ap-northeast-1.graphcms.com/v2/ckddnxngeiqt401xs38fje4s8/master",
+      },
+    },
+    {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
   	  options: {
         fonts: [
