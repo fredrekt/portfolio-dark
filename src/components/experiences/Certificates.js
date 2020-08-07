8c9ea958ef56e65d5559f168885866cc8d8296a4
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-08-08 00:44:48 +0800

    now graphql ready

---
+++ src/components/experiences/Certificates.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { StaticQuery } from 'gatsby';
+import { MDBCol } from 'mdbreact'
+import Moment from 'react-moment';
+
+const Certificates = ({theme}) => {
+    const color = {
+        borderLeft: `${theme === 'light' ? "1px solid #000" : "1px solid #fff"}`
+    }
+
+    return (
+        <StaticQuery 
+            query={graphql`
+                query{
+                    gcms{
+                        certificates(orderBy: date_DESC){
+                            date
+                            company
+                            title
+                            description
+                        }
+                    }
+                }
+            `}
+            render={data => (
+                <>
+                {data.gcms.certificates.map(certificate => 
+                <MDBCol className="my-4" md="12" lg="12">
+                    <div style={color} className="resume-container">
+                        <div style={{ marginLeft: `1rem` }} className="resume-content">
+                            <h2>
+                                <Moment format="YYYY">
+                                    {certificate.date}
+                                </Moment>
+                            </h2>
+                            <h4>{certificate.company}</h4>
+                            <h5>{certificate.title}</h5>
+                            <ul className="list-unstyled skills-list">
+                                {certificate.description.map(desc => <li>{desc}</li>)}
+                            </ul>
+                        </div>
+                    </div>
+                </MDBCol>)}
+                </>
+            )}
+        />
+    )
+}
+
+export default Certificates
---
+++ src/components/experiences/Experience.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { StaticQuery } from 'gatsby';
+import { MDBCol } from 'mdbreact'
+import Moment from 'react-moment';
+
+const Experience = ({theme}) => {
+    const color = {
+        borderLeft: `${theme === 'light' ? "1px solid #000" : "1px solid #fff"}`
+    }
+
+    const compareDates = (startDate, endDate) => {
+        const startingDate = ( <Moment format="YYYY">{startDate}</Moment> )
+        const endingDate = ( <Moment format="YYYY">{endDate}</Moment> )
+
+        if(startDate.toString() === endDate.toString()){
+           return(
+               <>
+                <h2>{startingDate}</h2>
+               </>
+           )
+        }
+        else{
+            return(
+                <>
+                <h2>
+                    {startingDate} 
+                    <span> - </span>
+                    {endingDate}
+                </h2>
+                </>
+            )
+        }
+    }
+    
+    return (
+        <StaticQuery 
+            query={graphql`
+                query{
+                    gcms{
+                        experiences(orderBy: startDate_DESC){
+                            current
+                            startDate
+                            endDate
+                            company
+                            job
+                            jobDescription
+                        }
+                    }
+                }
+            `}
+            render={data => (
+                <>
+                {data.gcms.experiences.map(experience => 
+                <MDBCol className="my-4" md="12" lg="12">
+                    <div style={color} className="resume-container">
+                        <div style={{ marginLeft: `1rem` }} className="resume-content">
+                            {experience.current && (<h2>Current</h2>)}
+                            {experience.startDate !== null && experience.endDate !==null && compareDates(experience.startDate, experience.endDate)}
+                            <h4>{experience.company}</h4>
+                            <h5>{experience.job}</h5>
+                            <ul className="list-unstyled skills-list">
+                                {experience.jobDescription.map(desc => <li>{desc}</li>)}
+                            </ul>
+                        </div>
+                    </div>
+                </MDBCol>)}
+                </>
+            )}
+        />
+    )
+}
+
+export default Experience
