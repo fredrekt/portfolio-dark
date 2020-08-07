150987e2069b466c9e2c45db4894c22003876353
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-08-08 00:50:04 +0800

    resume page now ready - refactored and cleaned up

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -13,6 +13,8 @@ import Design from '../components/skills/Design';
 import Languages from '../components/skills/Languages';
 import LeftMisc from '../components/skills/LeftMisc';
 import RightMisc from '../components/skills/RightMisc';
+import Experience from '../components/experiences/Experience';
+import Certificates from '../components/experiences/Certificates';
 
 const THEME = {
     light: 'light',
@@ -44,115 +46,13 @@ const ResumePage = () => {
                     <MDBCol md="4" lg="4">
                         <h1>Certificates</h1>
                         <MDBRow>
-                            <MDBCol className="my-4" md="12" lg="12">
-                                <div style={color} className="resume-container">
-                                    <div style={{ marginLeft: `1rem` }} className="resume-content">
-                                        <h2>2020</h2>
-                                        <h4>Udemy</h4>
-                                        <h5>Advanced React Patterns</h5>
-                                        <ul className="list-unstyled skills-list">
-                                            <li>Understood Design Patterns</li> 
-                                            <li>Becoming Senior React Developer</li>
-                                            <li>Master React Components</li>
-                                            <li>Built custom hooks</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </MDBCol>
-                            <MDBCol className="my-4" md="12" lg="12">
-                                <div style={color} className="resume-container">
-                                    <div style={{ marginLeft: `1rem` }} className="resume-content">
-                                        <h2>2020</h2>
-                                        <h4>Udemy</h4>
-                                        <h5>Machine Learning with JavaScript</h5>
-                                        <ul className="list-unstyled skills-list">
-                                            <li>Assembled machine learning algorithms</li> 
-                                            <li>Built applications using ml techniques</li>
-                                            <li>Learned Tensorflow JS</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </MDBCol>
-                            <MDBCol className="my-4" md="12" lg="12">
-                                <div style={color} className="resume-container">
-                                    <div style={{ marginLeft: `1rem` }} className="resume-content">
-                                        <h2>2020</h2>
-                                        <h4>Udemy</h4>
-                                        <h5>Angular - The Complete Guide</h5>
-                                        <ul className="list-unstyled skills-list">
-                                            <li>Learned TypeScript</li>
-                                            <li>Built Angular Web Applications</li>
-                                            <li>Became a MEAN Stack </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </MDBCol>
-                            <MDBCol className="my-4" md="12" lg="12">
-                                <div style={color} className="resume-container">
-                                    <div style={{ marginLeft: `1rem` }} className="resume-content">
-                                        <h2>2019</h2>
-                                        <h4>Impact Hackathon</h4>
-                                        <h5>Front-End Developer</h5>
-                                        <ul className="list-unstyled skills-list">
-                                            <li>Built frontend less than 8 hours</li> 
-                                            <li>Used Roads, Paypal & Geolocation API's</li>
-                                            <li>Automated process of Vehicle Clamping</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </MDBCol>
+                            <Certificates theme={theme}/>
                         </MDBRow>
                     </MDBCol>
                     <MDBCol md="4" lg="4">
                         <h1 className="mb-4">Experience</h1>
                         <MDBRow>
-                            <MDBCol className="my-4" md="12" lg="12">
-                                <div style={color} className="resume-container">
-                                    <div style={{ marginLeft: `1rem` }} className="resume-content">
-                                        <h2>Current</h2>
-                                        <h4>Upwork</h4>
-                                        <h5>Full Stack Developer</h5>
-                                        <ul className="list-unstyled skills-list">
-                                            <li>MERN Stack Development</li> 
-                                            <li>CMS Development</li>
-                                            <li>Landing Page Creator</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </MDBCol>
-                            <MDBCol className="my-4" md="12" lg="12">
-                                <div style={color} className="resume-container">
-                                    <div style={{ marginLeft: `1rem` }} className="resume-content">
-                                        <h2>2019</h2>
-                                        <h4>Freelancer</h4>
-                                        <h5>Full Stack Developer</h5>
-                                        <ul className="list-unstyled skills-list">
-                                            <li>Created CRUD Applications</li>
-                                            <li>React Developer</li>
-                                            <li>PHP Developer</li> 
-                                            <li>Programming Tutor</li>
-                                            <li>Code Debugger</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </MDBCol>
-                            <MDBCol className="my-4" md="12" lg="12">
-                                <div style={color} className="resume-container">
-                                    <div style={{ marginLeft: `1rem` }} className="resume-content">
-                                        <h2>2019 - 2020</h2>
-                                        <h4>Symph</h4>
-                                        <h5>Full Stack Developer Intern</h5>
-                                        <ul className="list-unstyled skills-list">
-                                            <li>Worked on frontend</li> 
-                                            <li>Learned ReactJS</li>
-                                            <li>Google App Engine</li>
-                                            <li>Worked on Government Digital Forms</li>
-                                            <li>Helped build landing pages</li>
-                                            <li>Hackathon: Facial Recoginition Application</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </MDBCol>
+                            <Experience theme={theme}/>
                         </MDBRow>
                     </MDBCol>
                     <MDBCol md="4" lg="4">
@@ -162,14 +62,7 @@ const ResumePage = () => {
                                 <div style={color} className="resume-container">
                                     <div style={{ marginLeft: `1rem` }} className="resume-content">
                                         <h4>Frontend</h4>
-                                        {/* <h5>Full Stack Developer</h5> */}
                                         <ul className="list-unstyled skills-list">
-                                            {/* <li>Sass</li> 
-                                            <li>CSS</li>
-                                            <li>Material Bootstrap</li>
-                                            <li>Styled Components</li>
-                                            <li>Styletron</li>
-                                            <li>Tailwind CSS</li> */}
                                             <Frontend/>
                                         </ul>
                                     </div>
@@ -178,16 +71,8 @@ const ResumePage = () => {
                             <MDBCol className="my-4" md="6" lg="6">
                                 <div style={color} className="resume-container">
                                     <div style={{ marginLeft: `1rem` }} className="resume-content">
-                                        {/* <h2>2017 - 2020</h2> */}
                                         <h4>Backend</h4>
-                                        {/* <h5>Full Stack Developer</h5> */}
                                         <ul className="list-unstyled skills-list">
-                                            {/* <li>NodeJS</li> 
-                                            <li>Php</li>
-                                            <li>Python</li>
-                                            <li>Strapi</li>
-                                            <li>Contentful</li>
-                                            <li>Airtable</li> */}
                                             <Backend/>
                                         </ul>
                                     </div>
@@ -196,22 +81,8 @@ const ResumePage = () => {
                             <MDBCol className="my-4" md="6" lg="6">
                                 <div style={color} className="resume-container">
                                     <div style={{ marginLeft: `1rem` }} className="resume-content">
-                                        {/* <h2>2017 - 2020</h2> */}
                                         <h4>Framework & Libraries</h4>
-                                        {/* <h5>Full Stack Developer</h5> */}
                                         <ul className="list-unstyled skills-list">
-                                            {/* <li>ReactJS</li> 
-                                            <li>React Native</li>
-                                            <li>Redux</li>
-                                            <li>GatsbyJS</li>
-                                            <li>AngularJS</li>
-                                            <li>VueJS</li>
-                                            <li>ExpressJS</li>
-                                            <li>Django</li>
-                                            <li>Spring Boot</li>
-                                            <li>Laravel</li>
-                                            <li>Flutter</li>
-                                            <li>NextJS</li> */}
                                             <Frameworks/>
                                         </ul>
                                     </div>
@@ -220,20 +91,8 @@ const ResumePage = () => {
                             <MDBCol className="my-4" md="6" lg="6">
                                 <div style={color} className="resume-container">
                                     <div style={{ marginLeft: `1rem` }} className="resume-content">
-                                        {/* <h2>2017 - 2020</h2> */}
                                         <h4>Data Handling</h4>
-                                        {/* <h5>Full Stack Developer</h5> */}
                                         <ul className="list-unstyled skills-list">
-                                            {/* <li>MongoDB</li>
-                                            <li>GraphQL</li> 
-                                            <li>Firestore</li>
-                                            <li>PostgreSQL</li>
-                                            <li>REST API</li>
-                                            <li>JSON</li>
-                                            <li>MySQL</li>
-                                            <li>SQLite</li>
-                                            <li>Firebase</li>
-                                            <li>Apollo GraphQL</li> */}
                                             <DataHandling/>
                                         </ul>
                                     </div>
@@ -242,15 +101,8 @@ const ResumePage = () => {
                             <MDBCol className="my-4" md="6" lg="6">
                                 <div style={color} className="resume-container">
                                     <div style={{ marginLeft: `1rem` }} className="resume-content">
-                                        {/* <h2>2017 - 2020</h2> */}
                                         <h4>Design</h4>
-                                        {/* <h5>Full Stack Developer</h5> */}
                                         <ul className="list-unstyled skills-list">
-                                            {/* <li>Photoshop</li> 
-                                            <li>Adobe XD</li>
-                                            <li>Figma</li>
-                                            <li>Tailblocks</li>
-                                            <li>Lightroom</li> */}
                                             <Design/>
                                         </ul>
                                     </div>
@@ -259,15 +111,8 @@ const ResumePage = () => {
                             <MDBCol className="my-4" md="6" lg="6">
                                 <div style={color} className="resume-container">
                                     <div style={{ marginLeft: `1rem` }} className="resume-content">
-                                        {/* <h2>2017 - 2020</h2> */}
                                         <h4>Languages</h4>
-                                        {/* <h5>Full Stack Developer</h5> */}
                                         <ul className="list-unstyled skills-list">
-                                            {/* <li>Javascript</li> 
-                                            <li>TypeScript</li>
-                                            <li>Python</li>
-                                            <li>PHP</li>
-                                            <li>Java</li> */}
                                             <Languages/>
                                         </ul>
                                     </div>
@@ -282,31 +127,11 @@ const ResumePage = () => {
                                                 <MDBRow>
                                                     <MDBCol md="6" lg="6">
                                                         <ul className="list-unstyled skills-list">
-                                                            {/* <li>Git</li> 
-                                                            <li>Machine Learning</li>
-                                                            <li>Jamstack</li>
-                                                            <li>Composer</li>
-                                                            <li>PayMongo</li>
-                                                            <li>Heroku</li>
-                                                            <li>Github</li>
-                                                            <li>Gatsby Cloud</li>
-                                                            <li>Google App Engine</li>
-                                                            <li>Netlify</li> */}
                                                             <LeftMisc/>
                                                         </ul>
                                                     </MDBCol>
                                                     <MDBCol md="6" lg="6">
                                                         <ul className="list-unstyled skills-list">
-                                                            {/* <li>Docker</li> 
-                                                            <li>Wondershare Filmora</li>
-                                                            <li>Kubernetes</li>
-                                                            <li>Blockchain</li>
-                                                            <li>Shopify</li>
-                                                            <li>GraphCMS</li>
-                                                            <li>Prismic</li>
-                                                            <li>Yarn</li>
-                                                            <li>CLI</li>
-                                                            <li>NPM</li> */}
                                                             <RightMisc/>
                                                         </ul>
                                                     </MDBCol>
