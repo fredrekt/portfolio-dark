41c6e15b8b5f98733e3488b06cc9326ee0bda93a
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-08-27 19:52:11 +0800

    added graphql

--- src/components/experiences/Experience.js
+++ src/components/experiences/Experience.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StaticQuery } from 'gatsby';
+import { StaticQuery, graphql } from 'gatsby';
 import { MDBCol } from 'mdbreact'
 import Moment from 'react-moment';
 
