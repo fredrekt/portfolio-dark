82af9b1441c6e21655a19c0ff5a7cabaad8cb229
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-22 22:33:11 +0800

    added initial state

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -12,7 +12,7 @@ const THEME = {
 };
 
 const BlogPage = () => {
-    const initialColor = () => typeof window !== `undefined` && window.localStorage.getItem('themeColor')
+    const initialColor = () => typeof window !== `undefined` && window.localStorage.getItem('themeColor') || 'light'
     const [theme, setTheme] = useState(initialColor);
 
     useEffect(() => {
