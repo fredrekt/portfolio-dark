394fa104925bc46c3b39e6af38ce4e77bf9a8d52
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-22 17:57:49 +0800

    using createPages api

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,22 @@
 const path = require('path');
 
-exports.onCreatePage = async ({ page, actions }) => {
-    const { createPage } = actions
+exports.createPages = async ({ graphql, actions: { createPage } }) => {
+    const {data: {gcms: {blogs}}} = await graphql(`
+        {
+            gcms{
+                blogs{
+                    id
+                    title
+                }
+            }
+        }
+    `)
 
-    console.log("Page", page.path);
-    if(page.path.match(/^\/blog/)){
-        createPage({
-            path: "/blog",
-            matchPath: "/blog/*",
-            component: path.resolve(`src/pages/routes.js`)
-        })
-    }
+    blogs.forEach(({id, title}) => createPage({
+        path: `/blog/${id}`,
+        component: require.resolve(`./src/templates/BlogPage.js`),
+        context: {
+            id
+        }
+    }))
 }
