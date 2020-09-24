1ab7d3013fcfa6b375f000e02ed1d13540200661
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-24 13:12:21 +0800

    mdx images now responsive

--- src/components/blog/BlogBody.js
+++ src/components/blog/BlogBody.js
@@ -6,7 +6,7 @@ const BlogBody = ({content}) => {
     return (
         <>
         <MDBCol md='8' lg='8'>
-            <Markdown className="h3-responsive">
+            <Markdown options={{ overrides: { img: { props: { className: 'img-fluid' } } } }} className="h3-responsive">
                 {content}
             </Markdown>
         </MDBCol> 
